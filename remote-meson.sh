df2a005e9a19827b0947f0b36baf24e254f98a0b
ch8o <cyb2952873@gmail.com>
2021-08-25 16:22:25 +0800

    Create remote-meson.sh

---
+++ remote-meson.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+  
+destip=$1
+if [ -z "$destip" ]
+then
+       echo 'arg1: destip'
+       exit -1
+fi
+cmd="
+sudo wget 'https://raw.githubusercontent.com/ch8o/rawfile/main/meson.sh'
+sudo wget 'https://raw.githubusercontent.com/ch8o/rawfile/main/spawn.sh'
+
+sudo chmod +x meson.sh
+sudo chmod +x spawn.sh
+
+sudo ./meson.sh
+"
+echo $cmd
+
+ssh -o StrictHostKeyChecking=no -i aws.key ubuntu@$destip "$cmd"
