5b04beb83753dc775415cf5406fae07754d3a8a5
ch8o <cyb2952873@gmail.com>
2021-08-26 17:29:10 +0800

    Update spawn.sh

--- spawn.sh
+++ spawn.sh
@@ -7,9 +7,9 @@ send "p1LjdTyqASPMVTNo6MKR+w==\r"
 
 expect "Please enter your port,CAN NOT be 80 or 443(default 19091)"
 send "\r"
-
+set size [lindex $argv 0];
 expect "40GB disk space is the minimum, default will be 80GB. Please make sure you have enough free space"
-send '$1\r'
+send '$size\r'
 
 expect eof
 expect eof
