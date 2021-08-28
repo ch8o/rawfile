a48ddf594fc63d7c2fc025d577736c442b77025f
ch8o <cyb2952873@gmail.com>
2021-08-28 22:00:58 +0800

    Update spawn.sh

--- spawn.sh
+++ spawn.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/expect
 
+set size [lindex $argv 0];
+set token [lindex $argv 1];
+
 cd ./meson-linux-amd64
 spawn sudo ./meson service-install
 expect "Please enter your token"
-send "p1LjdTyqASPMVTNo6MKR+w==\r"
+send "$token\r"
 
 expect "Please enter your port,CAN NOT be 80 or 443(default 19091)"
 send "\r"
 
-set size [lindex $argv 0];
 expect "40GB disk space is the minimum, default will be 80GB. Please make sure you have enough free space"
 send "$size\r"
 
